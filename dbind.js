6a6f5dafe13335a00a270b43f8fc19567ef2800e
Kris Zyp <kriszyp@gmail.com>
2012-02-16 19:32:26 -0700

    Start on leveraging CSS for styling

--- dbind.js
+++ dbind.js
@@ -10,6 +10,12 @@ define(['./Reactive', './Cascade', './ReactiveObject', './parser', 'put-selector
 	}
 	var strings = ["green", "bold"];
 	var divStyle = put("div").style;
+	var ua = navigator.userAgent;
+	var vendorPrefix = ua.indexOf("WebKit") > -1 ? "-webkit-" :
+		ua.indexOf("Firefox") > -1 ? "-moz-" :
+		ua.indexOf("MSIE") > -1 ? "-ms-" :
+		ua.indexOf("Opera") > -1 ? "-o-" : "";
+
 	var domContext = new Reactive;
 	var DOMElement = Compose(Reactive, {
 		then: function(callback){
@@ -17,20 +23,31 @@ define(['./Reactive', './Cascade', './ReactiveObject', './parser', 'put-selector
 			var element = this.element;
 			callback({
 				create: function(cascade){
+					var parent = cascade.parent;
 					if(!element){
 						element = cascade.element || (cascade.element = put(selector));
+						element.className = getCSSClass(parent);
 					}
-					var parent = cascade.parent;
 					var children = parent.children;
 					if(children){
 						for(var i = 0; i < children.length; i++){
-							children[i].get("element").then(function(child){
-								if(typeof child == "string"){
-									// eventually do this for any non-element, I think
-									child = document.createTextNode(child);
-								}
-								element.appendChild(child);
-							});
+							(function(child){
+								var lastChildElement;
+								child.get("element").then(function(childElement){
+									if(lastChildElement){
+										element.removeChild(lastChildElement);
+									}
+									lastChildElement = childElement;
+									if(childElement){
+										element.appendChild(childElement);
+									}else{
+										// if there is no element wrapper, we just get the main value and insert it as a plain text node
+										child.then(function(value){
+											element.appendChild(document.createTextNode(value));
+										});
+									}
+								})
+							})(children[i]);
 						}
 					}else{
 						parent.then(function(value){
@@ -50,6 +67,24 @@ define(['./Reactive', './Cascade', './ReactiveObject', './parser', 'put-selector
 								}
 							}
 						});
+					}
+					function getCSSClass(cascade, callback){
+						cascade.eachBase(function(base){
+							
+						});
+						var selector = getCSSClass(cascade.parent) + "-" + cascade.key;
+						cascade.keys(function(child){
+							var style = (sheet.cssRules || sheet.rules)[sheet.addRule ?
+								sheet.addRule(selector, "") : sheet.insertRule(selector + "{}", this.cssRules.length)].style;
+							var key = child.key;
+							if(key in style || (key = vendorPrefix + key) in style){
+								child.then(function(value){
+									style[key] = value;
+								}); 
+							}
+							
+						});
+						return selector;
 					}
 					parent.keys(function(child){
 						if(child.key in divStyle){
@@ -80,14 +115,21 @@ define(['./Reactive', './Cascade', './ReactiveObject', './parser', 'put-selector
 		domContext[i + '-'] = cascade;
 	}
 	function dbind(element, data, sheet){
+		var root = createRoot(element);
+		if(data){
+			root.get("source").extend(new ReactiveObject(data));
+		}
+		parser({text: sheet}, root);
+		return root.get("element");
+	}
+	function createRoot(element){
 		var root = new Cascade;
 		var rootElement = new DOMElement;
 		rootElement.element = element;
 		root.get("element").extend(rootElement);
 		root.parent = domContext;
-		root.get("source").extend(new ReactiveObject(data));
-		parser(sheet, root);
-		return root.get("element");
+		return root;
 	}
+	dbind.createRoot = createRoot;
 	return dbind;
 });
