499c5901132fb1bc9e867a02f079eeeb1a774268
Kris Zyp <kriszyp@gmail.com>
2012-03-26 12:36:27 -0600

    Support recursive, parameterized, asynchronous override functions

--- Cascade.js
+++ Cascade.js
@@ -8,28 +8,6 @@ define([], function(){
 	function Cascade(){
 	}
 	Cascade.prototype = {
-		get: function(key, callback){
-			// get the reactive child by the given key
-			if(key in this){
-				var child = this[key];
-			}else{
-				(this.keySet || (this	.keySet = [])).push(key);
-				var child = this[key] = new Cascade;
-				child.parent = this;
-				child.key = key;
-				var bases = this.bases;
-				if(bases){
-					for(var i = 0; i < bases.length; i++){
-						// TODO: can instanceChain be just a number?
-						extend(get(bases[i], key), child, this.instanceChain ? [this].concat(this.instanceChain) : [this]);
-					}
-				}
-			}
-			if(callback){
-				child.then ? child.then(callback) : callback(child);
-			}
-			return child;
-		},
 		addRef: function(ref){
 			(this._refs || (this._refs = [])).push(ref);
 		},
@@ -65,8 +43,10 @@ define([], function(){
 				return waitingOn.shift()(proceed);
 			}
 			var bases = this.resolveBases();
+			var refs = this._refs;
 			if(this.bases){
 				bases = this.bases.concat(bases);
+				refs = Array(this.bases.length).concat(refs); // keep the ordering of the refs in sync with the bases
 			}
 			var waiting = 1;
 			if(!bases.length){
@@ -76,18 +56,30 @@ define([], function(){
 			
 			for(var i = 0; i < bases.length; i++){
 				var base = bases[i];
-				base.whenReady ?
-					 waiting++ && base.whenReady(done(base)) :
-					 extend(base, this);
+				var args = (refs[i] || 0).args;
+				if(base.whenReady){
+					// TODO: eliminate whenReady and use promises from override instead
+					waiting++; 
+					base.whenReady(done(base, args));
+				}else{
+					 var result = extend(base, this, null, args);
+					 if(result && result.then){
+					 	waiting++;
+					 	result.then(done());
+					 }
+				}
 			}
 			this.whenReady = null;
 			done()();
 			function proceed(){
 				self.whenReady();
 			}
-			function done(target){
+			function done(target, args){
 				return function(){
-					target && extend(target, self);
+					var result = target && extend(target, self, null, args);
+					if(result && result.then){
+						return result.then(done());
+					}
 					waiting--;
 					if(!waiting){
 						var callbacks = self.readyCallbacks;
@@ -182,6 +174,26 @@ define([], function(){
 					listener(this[key]);
 				}
 			}		
+		},
+		override: function(target){
+			// recursively execute override function for the bases
+			var bases = this.bases;
+			if(bases){
+				for(var i = 0; i < bases.length; i++){
+					var base = bases[i];
+					if(base.override){
+						base.override(target);
+					}
+				}
+			}
+			if(this.getValue){
+				target.getValue = this.getValue;
+			}
+
+			if(this.put){
+				target.put = this.put;
+			}
+			
 		}
 	};
 /*
@@ -206,14 +218,15 @@ define([], function(){
 	
 	var get = Cascade.get = function(target, key, callback){
 		if(target.get){
-			return target.get(key, callback);
-		}
+			var child = target.get(key);
+		}else if(key in target){
 		// get the reactive child by the given key
-		if(key in target){
 			var child = target[key];
 		}else{
-			(target.keySet || (target	.keySet = [])).push(key);
+			(target.keySet || (target.keySet = [])).push(key);
 			var child = target[key] = new Cascade;
+		}
+		if(!child.key){
 			child.parent = target;
 			child.key = key;
 			var bases = target.bases;
@@ -225,6 +238,9 @@ define([], function(){
 			}
 		}
 		if(callback){
+			if(typeof callback == "string"){
+				return get.apply(null, [child].concat([].slice.call(arguments, 2)));
+			}
 			child.then ? child.then(callback) : callback(child);
 		}
 		return child;
@@ -232,7 +248,7 @@ define([], function(){
 	Cascade.addBase = function(base, target){
 		(base._refs || (base._refs = [])).push(target);
 	};
-	var extend = function(base, target, targets){
+	var extend = function(base, target, targets, args){
 		if(typeof base != "object"){
 			return base !== undefined && target.is(base);
 		}
@@ -248,8 +264,9 @@ define([], function(){
 		(target.bases || (target.bases = [])).push(base);
 		if(base.override){
 			// allow for more sophisticated overrides than simply getValue replacemetns
-			base.override(target);
+			var result = base.override(target, args);
 		}else{
+			// TODO: this might be eliminitable
 			if(base.getValue){
 				target.getValue = base.getValue;
 			}
@@ -267,198 +284,17 @@ define([], function(){
 				extend(get(base, key), target[key], targets ? [target].concat(targets) : [target]);
 			}
 		}
+		return result;
 	};
+	function startValue(target){
+		this.value = {};
+		var oldGetValue = this.getValue;
+		this.getValue = function(){
+			return this.value.getValue();
+		}
+		return oldGetValue;
+	}
+	
 	return Cascade;
 	 	
 });
-
-/*define(['./Reactive'], function(Reactive){
-	// Cascading inheritance
-	var nextId = 1;
-
-
-	function Cascade(){
-	}
-	var CascadePrototype = Cascade.prototype = new Reactive; 
-	CascadePrototype._createChild = function(key){
-		var child = new Cascade();
-		var listeners = this.keyListeners;
-		if(listeners){
-			for(var i = 0, l = listeners.length;i < l; i++){
-				var listener = listeners[i];
-				listener(child);
-			}
-		}
-		return child;
-	};
-	CascadePrototype.put = function(value){
-		// put the main value in this reactive, comes from the user, 
-		// propagates down (change event propagates back up), by default
-		// we send it to our first base (hopefully there isn't more bases)
-		this.eachBase(function(base){
-			if(base.put){
-				base.put(value);
-				return true;
-			}
-		});
-	};
-	CascadePrototype.set = function(name, value){
-		var reactive = new Reactive;
-		reactive.is(value);
-		this.get(name).put(reactive);
-	}
-	CascadePrototype.create = function(){
-		// creates a new instance of this Reactive
-		var instance = new Reactive(this.ast, parentScope);
-		instance.bases = [this];
-	};
-	CascadePrototype.call = function(){
-		// when this is called with consecutive arguments, we apply them to the keys in the order they appear
-		var cascade = new Cascade;
-		cascade.extend(this);
-		var args = arguments, i = 0;
-		this.keys(function(key){
-			cascade.get(key).is(args[i++]);
-			return i >= args.length; // tell it we are done
-		});
-	};
-	CascadePrototype.resolve = function(baseName){
-		var lastParent = this;
-		var target;
-		var parent;
-		while(parent = lastParent.parent){
-			// TODO: Wait for the parent scope to be whenReady
-			// search through scopes until we find the base's name
-			if(parent[baseName + '-']){
-				return parent;
-			}
-			lastParent = parent;
-		}
-		// we couldn't find the base's name, create it at the global level
-		console.log(baseName + " reference not found"); // wait until the global is fully created before giving this error message in case it is declared later
-		return lastParent;
-		
-	};
-	CascadePrototype.extend = function(base, path){
-		if(path){
-			base = {parent: base, path: path};
-		}
-		// the derivitive of my base is me 
-		(this.bases || (this.bases = [])).push(base);
-		// for each base, we extend all the children that have been cached
-		var self = this;
-		// TODO: fire base listeners
-		var baseListeners = this.baseListeners || 0;
-		for(var i = 0; i < baseListeners.length; i++){
-			baseListeners[i](base);
-		}
-		// TODO: do the conditional search for the primary value through the bases
-	};
-	
-	CascadePrototype.waitFor = function(promise){
-		(this.waitingOn || (this.waitingOn = [])).push(promise);
-	};
-	CascadePrototype.then = function(listener, errorHandler){
-		var listeners = this.listeners;
-		if(!listeners){
-			var self = this;
-			listeners = this.listeners = [];
-			// no listeners, setup the listening mechanism
-			var listeners = this.listeners;
-			this.listeningBase = this.bases ? this.bases.length : 0;
-			var nextHandle, thisListenerBase = this.listeningBase;
-			this.eachBase(function(base){
-				var done;
-		 		base.then(function(value){
-					if(value === undefined){
-						if(thisListenerBase == self.listeningBase){
-							var nextListenerBase = --self.listeningBase;
-							if(nextListenerBase < 0){
-								self.listeningBase++; // revert the index change
-								self.is(); // call with undefined as the value
-							}
-						}
-					}else{
-						if(self.listeningBase < thisListenerBase){
-							// stop listening to the next bases if we don't need them
-							nextHandle && nextHandle.remove();
-						}
-						if(value && value.create){
-							value = value.create(self);
-						}
-						// when a new value is provided, we notify all the listeners
-						self.is(value);
-						done = true;
-					}
-			 	});
-			 	return done;
-			}, 0, 0, true);
-		}
-		listeners.push(listener);
-		if("value" in this){
-			listener(this.value);
-		}
-	};
-	CascadePrototype.eachBase = function(callback, forInstance, forBases, excludeSelf, waitingOn){
-		if(!waitingOn){
-			waitingOn = this.waitingOn || [];
-			waitingOn.parent = this.parent;
-		}
-		function forBase(base){
-			var parent = base.parent;
-			var path = base.path;
-			if(path){
-				if(forInstance && forBases.indexOf(parent) > -1){ // Need to recurse
-					parent = forInstance;
-				}
-				base = parent;
-				for(var j = 0; j < path.length; j++){
-					base = base.get(path[j]);
-				}
-			}
-			if((!excludeSelf || !base.eachBase) && callback(base)){
-				return true;
-			}
-			if(base.eachBase && base.eachBase(callback, forInstance, null, true)){
-				return true;
-			}			
-		}
-		(this.baseListeners || (this.baseListeners = [])).push(forBase); 
-		var scope, args = arguments;
-		while(true){
-			if(waitingOn.length){
-				var next = waitingOn.shift();
-				var self = this;
-				return next(function(){
-					args[4] = waitingOn;
-					args.length = 5;
-					args.callee.apply(self, args);
-				});
-			}else if((scope = waitingOn.parent)){
-				waitingOn = scope.waitingOn || [];
-				waitingOn.parent = scope.parent;
-			}else{
-				// done waiting for all the necessary async actions to finish
-				var bases = this.bases || 0;
-				for(var i = bases.length; i > 0;){
-					i--;
-					if(forBase(bases[i])){
-						return true;
-					}
-				}
-				var key = this.key;
-				var parent = this.parent;
-				var parents = parent && [];
-				return parent && parent.eachBase &&parent.eachBase(function(parentBase){
-					parents.push(parentBase);
-					var nextBase = parentBase.get(key);
-					return nextBase.eachBase ? nextBase.eachBase(function(base){
-						return callback(base);
-					}, parent, parents, true) : callback(nextBase);
-				});
-			}
-		}
-	};
-	return Cascade; 	
-});*/
-
