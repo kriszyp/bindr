e00c91d6e0458de2d48401d950cedd7138b6edd4
Kris Zyp <kriszyp@gmail.com>
2012-04-05 11:05:51 -0600

    Got reactive functions working

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 define(['./Reactive', './Cascade', 'compose/compose'], function(Reactive, Cascade, Compose){
 	return function(env){
 		var get = Cascade.get;
+		var resolve = Cascade.resolve;
 		var moduleBase = get(env, 'module');
 		moduleBase.override = function(target, args){
 			if(args){
@@ -20,10 +21,39 @@ define(['./Reactive', './Cascade', 'compose/compose'], function(Reactive, Cascad
 				};
 			}
 		};
-		get(env, 'or').override = function(target, args){
-			target.getValue = function(callback){
-				return "hi"
-			}
+		function reactiveFunction(target, func){
+			target.override = function(target, args){
+				target.getValue = function(callback){
+					var resolvedArgs = [];
+					var doneInit;
+					for(var i = 0; i < args.length; i++){
+						var arg = args[i];
+						(function(arg, i){
+							get(arg, function(value){
+								resolvedArgs[i] = value;
+								if(doneInit){
+									// only call this asyncronously
+									callback(func.apply(this, resolvedArgs));
+								}
+							});
+						})(arg.splice ? resolve(target, arg) : arg, i);
+					}
+					doneInit = true;
+					if(resolvedArgs.length){
+						// some have resolved now at least, call the function
+						callback(func.apply(this, resolvedArgs));
+					}
+				}
+			};
 		};
+		reactiveFunction(get(env, 'or'), function(a, b){
+			return a || b;
+		});
+		reactiveFunction(get(env, 'and'), function(a, b){
+			return a && b;
+		});
+		reactiveFunction(get(env, 'add'), function(a, b){
+			return a + b;
+		});
 	};
 });
